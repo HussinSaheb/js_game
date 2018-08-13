7b9d06854985370087a8439cba2975c5c4c7f9b5
Hussin Saheb <Hsaheb@spartaglobal.com>
2018-08-13 15:50:51 +0100

    wrote functions for display the guess, and display the player names

--- js/index.js
+++ js/index.js
@@ -1,6 +1,51 @@
-$.(function(){
+$(function(){
+  showHide();
+  addPlayers(2, ["hussin", "mav"]);
+  displayGuesses();
 
+  function askPlayerAmount() {
+    // when player enters the amount of Players
+    $("#getPlayers").on("keydown", function(){
+      //if enter has been pressed
+      if (event.keyCode == 13){
+        // create new input fields for player names
+        for (var i = 1; i <= $("#getPlayers").val(); i++) {
+          $(".getPlayersForm").append("<input type='text' name='"+i+"' class='playerName' placeholder='player name"+ i +"'>");
+        }
+      }
+      $(".playerName").css({
+        border:"1px solid black",
+        display:"Block;"
+      })
+    })
+  }
+  // implement  dynamic show and hide function
+  function showHide() {
+    $(".game").hide();
+  }
+  // add function to populate the player section of the game
+  function addPlayers(num,arrayNames) {
+    var personName = "";
+    // create player div
+    for (var i = 0; i < num; i++) {
+      personName= arrayNames[i];
+      // add div with person name to player columns
+      $(".playerDisplay").append("<div class='player'></div>");
+    }
+    $(".player").each(function(index){
+      $(this).text(arrayNames[index]);
+    })
+  }
 
-
-  
+  // displays the guesse from the input above in the same column
+  function displayGuesses() {
+    $("#guess").on("keydown",function(event){
+      if (event.keyCode == 13){
+        $(".guesses").append("<p>"+ $("#guess").val() +"</p>");
+      }
+    })
+    $("form").on("submit",function(event){
+      event.preventDefault();
+    })
+  }
 })
