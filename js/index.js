b6e8efbe6d835c60047027c34ca9d41a4f15bb15
Hussin Saheb <Hsaheb@spartaglobal.com>
2018-08-15 12:27:40 +0100

    added comparison of words and updating score

--- js/index.js
+++ js/index.js
@@ -1,56 +1,17 @@
 $(function(){
+  var drawWord = new Word();
+  // hide the game div at start
   $(".game").hide();
   startGame();
   var leaderboard = new Leaderboard();
+  // start a game
   function startGame() {
     askPlayerAmount();
     displayGuesses();
     displayWord();
-    drawcanvas();
+    drawCanvas();
   }
-  function drawcanvas() {
-    var canvas = $("#canvas")[0].getContext('2d');
-    // used to check if the mouse has been pressed or not
-    var isDrawing;
-    canvas.fillCircle = function(x, y, radius, fillColor) {
-      this.fillStyle = fillColor;
-      this.beginPath();
-      this.moveTo(x, y);
-      this.arc(x, y, radius, 0, Math.PI * 2, false);
-      this.fill();
-    };
-    // compares the canvas
-    function getMousePos(canvas, e) {
-      var rect =  $("#canvas")[0].getBoundingClientRect();
-      return {x: e.clientX - rect.left, y: e.clientY - rect.top};
-    }
-    // if the mouse is pressed we draw
-    $("#canvas").mousedown(function(event){
-      isDrawing = true;
-    })
-    //draw from the press to the nd
-    $("#canvas").mousemove(function(event){
-      if (!isDrawing) {
-        return;
-      }
-      // get the mouse positon in relation to the canvas
-      var pos = getMousePos(canvas, event);
-      // assign x to the mouse
-      var x =  pos.x;
-      // assign y to mouse
-      var y = pos.y;
-      // set a radius of the drawn circles
-      var radius = 1;
-      // give it a colour
-      var fillColor = '#ff0000';
-      // call the function to draw
-      canvas.fillCircle(x, y, radius, fillColor);
-    })
-    // if the mouse is released we stop the draw
-    $("#canvas").mouseup(function(event){
-      isDrawing = false;
-    })
-  }
+  // function to retrieve the values from inputs
   function askPlayerAmount() {
     var playerArray = [];
     var numberOfPlayers;
@@ -95,21 +56,85 @@ $(function(){
       //create a new player object
       // add the players name and score
       var person =  new Player(playerNames[index], 0);
-      // display in the window the players name and score
-      $(this).text(playerNames[index]  + "|" + person.getScore());
+      // display in the window the players name.
+      $(this).text(playerNames[index]);
       // add the new player to the leaderboard
       leaderboard.addEntry(person);
     })
-
+    // call leaderboard to be displayed
     displayLeaderboard(leaderboard);
     $(".mainMenu").hide();
     $(".game").show();
+  }
+  // display leaderboard on page
+  function displayLeaderboard(leaderboard) {
+    $(".leaderboard").append("<ul id='leaderboard'></ul>");
+    for (var i = 0; i < leaderboard.getScores().length; i++) {
+      $("#leaderboard").append("<li id="+leaderboard.getScores()[i].getName()+">"+leaderboard.getScores()[i].getName() +  "|"+leaderboard.getScores()[i].getScore()+"</li>")
+    }
+  }
+  // displays the word on page//sets the objct word
+  function displayWord() {
+    // get word from the datamuse api using get restful
+    $.get("https://api.datamuse.com/words?ml=&max=999&topics=furniture", function(result){
+      // get a random number
+      var number  = Math.floor(Math.random() * 30);
+      // set the number to pick one from 999 words
+      $("#words").text(result[number].word);
+      // set the word to word object
+      drawWord.setWord(result[number].word);
+    })
+  }
+  // function to show and deal with the canvas inputs
+  function drawCanvas() {
+    var canvas = $("#canvas")[0].getContext('2d');
+    // used to check if the mouse has been pressed or not
+    var isDrawing;
+    canvas.fillCircle = function(x, y, radius, fillColor) {
+      this.fillStyle = fillColor;
+      this.beginPath();
+      this.moveTo(x, y);
+      this.arc(x, y, radius, 0, Math.PI * 2, false);
+      this.fill();
+    };
+    // compares the canvas
+    function getMousePos(canvas, e) {
+      var rect =  $("#canvas")[0].getBoundingClientRect();
+      return {x: e.clientX - rect.left, y: e.clientY - rect.top};
+    }
+    // if the mouse is pressed we draw
+    $("#canvas").mousedown(function(event){
+      isDrawing = true;
+    })
+    //draw from the press to the nd
+    $("#canvas").mousemove(function(event){
+      if (!isDrawing) {
+        return;
+      }
+      // get the mouse positon in relation to the canvas
+      var pos = getMousePos(canvas, event);
+      // assign x to the mouse
+      var x =  pos.x;
+      // assign y to mouse
+      var y = pos.y;
+      // set a radius of the drawn circles
+      var radius = 1;
+      // give it a colour
+      var fillColor = '#ff0000';
+      // call the function to draw
+      canvas.fillCircle(x, y, radius, fillColor);
+    })
+    // if the mouse is released we stop the draw
+    $("#canvas").mouseup(function(event){
+      isDrawing = false;
+    })
   }
   // displays the guesse from the input above in the same column
   function displayGuesses() {
     $("#guess").on("keydown",function(event){
       if (event.keyCode == 13){
-        $(".guesses").append("<p>"+ $("#guess").val() +"</p>");
+        $(".guesses").append("<p class='guessParagraph'>"+$("#guess").val()+"</p>");
+        compareGuess(leaderboard.getScores()[0]);
       }
     })
     // set the input box to the bottom of the page
@@ -121,43 +146,58 @@ $(function(){
       event.preventDefault();
     })
   }
-  function displayLeaderboard(leaderboard) {
-    $(".leaderboard").append("<ul id='leaderboard'></ul>");
-    for (var i = 0; i < leaderboard.getScores().length; i++) {
-      $("#leaderboard").append("<li>"+leaderboard.getScores()[i].getName() +  "|"+leaderboard.getScores()[i].getScore()+"</li>")
+  // compare a guess from a player and the word on screen
+  function compareGuess(Player) {
+        // check if the word is the last one entered
+        if ($(".guessParagraph:last-child").text() == drawWord.getWord()) {
+          // if correct assign the player 100 points
+          Player.score += 100;
+          $(".guessParagraph:last-child").css({
+            backgroundColor: "green"
+          })
+          // call to updte the leaderboard
+          updateScoreBoard(Player);
+        }else{
+          $(".guessParagraph:last-child").css({
+            backgroundColor: "red"
+          })
+        }
+      }
+  function updateScoreBoard(Player) {
+    $("#"+Player.getName()+"").text(""+Player.getName()+ "|" + Player.getScore());
+  }
+  // create Word Class
+  function Word(word) {
+    this.word = word;
+    Word.prototype.getWord = function() {
+      return this.word;
+    }
+    Word.prototype.setWord = function(word){
+      this.word = word;
     }
   }
-  function displayWord() {
-    // get word from the datamuse api using get restful
-    $.get("https://api.datamuse.com/words?ml=&max=999&topics=furniture", function(result){
-        $("#words").text(result[0].word);
-      })
-    }
-function Player(name, score) {
-  this.name = name;
-  this.score = score;
-  Player.prototype.getName = function () {
-    return this.name;
-  };
-  Player.prototype.getScore = function () {
-    return this.score;
-  };
-}
-function Leaderboard(){
-  this.scores = [];
-  Leaderboard.prototype.getScores = function () {
-    return this.scores
-  };
-  //change name as its confusing with the other function
-  Leaderboard.prototype.addEntry = function(Player) {
-    this.scores.push(Player);
+  //create the Player Class
+  function Player(name, score) {
+    this.name = name;
+    this.score = score;
+    // get the players name
+    Player.prototype.getName = function () {
+      return this.name;
+    };
+    //get the players score
+    Player.prototype.getScore = function () {
+      return this.score;
+    };
   }
-
-}
-
-
-
-
-
-
+  // create Leaderboard class
+  function Leaderboard(){
+    this.scores = [];
+    Leaderboard.prototype.getScores = function () {
+      return this.scores
+    };
+    //change name as its confusing with the other function
+    Leaderboard.prototype.addEntry = function(Player) {
+      this.scores.push(Player);
+    }
+  }
 })
